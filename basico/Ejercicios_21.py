b0dac79f5a9ee46c262b505a61a31df9d54ce0e7
IsabelManiega <isabelmaniega@hotmail.com>
2022-07-27 12:32:56 +0200

    Ejercicios 21 resueltos

--- basico/Ejercicios_21.py
+++ basico/Ejercicios_21.py
@@ -8,10 +8,16 @@
 """
 
 def es_bisiesto():
-    # TODO: bisiesto es divisible por 4
-    # TODO: bisiesto NO es divisible por 100
-    # TODO: bisiesto es divisible por 400
-    pass
+    print("Comprueba años bisiestos")
+    a = int(input("Escriba un años y le dire si es bisiesto: "))
+    if a % 4 == 0 and (not(a % 100 == 0)):
+        print("El año", a, "es un año bisiesto porque es multiplo de 4")
+    elif a % 400 == 0:
+        print("El año", a, "es un año bisiesto porque es multiplo de 400")
+    else:
+        print("El año", a, "no es bisiesto")
+
+es_bisiesto()
 
 # EJERCICIO 2
 
@@ -24,8 +30,21 @@ def es_bisiesto():
     sabiendo que una cantidad de 10000 dolares al 4.5% de interés anual se convierte en 24117.14 dolares al cabo de 20 años.
 """
 
-def hipoteca():
-    pass
+def calculo(dinero, inte, cant_anos):
+    x = dinero * ((1 + inte/100) **cant_anos)
+    return x
+
+def main():
+    dolares = int(input("Cuantos dolares: "))
+    interes = input("Cuanto interes: ")
+    interes = float(interes)
+    anos = int(input("Cantidad de años: "))
+    print("\n")
+
+    resultado = calculo(dolares, interes, anos)
+    print("Cuando pasen", anos, u"años, con un", interes, u"de interes, usted habrá generado", resultado, "dolares.")
+
+main()
 
 # EJERCICIO 3
 
@@ -42,6 +61,40 @@ def hipoteca():
 # ESTRENOS       3.50      3          1.00
 # SUPER ESTRENOS 4.00      4          1.50
 
-def main():
-    pass
+while True:
+    print("\n")
+    print("**************** MENU *******************")
+    print("*****************************************")
+    print("***********  1. Favoritos ***************")
+    print("***********  2. Nuevos    ***************")
+    print("***********  3. Estrenos  ***************")
+    print("***********  4. Super Estrenos  *********")
+    print("***********  99. Salir (del menú)  ******")
+    print("*****************************************")
+
+    print("\n")
+
+    opcion = int(input("Inserte su opción: "))
+
+    if opcion == 1:
+        n_dias = int(input("Inserte numero de días: "))
+        total = float(2.50 + (0.50 * n_dias))
+        print("EL TOTAL A PAGAR ES: $",total, "dolares")
+    elif opcion == 2:
+        n_dias = int(input("Inserte numero de días: "))
+        total = float(3.00+ (0.75 * n_dias))
+        print("EL TOTAL A PAGAR ES: $",total, "dolares")
+    elif opcion == 3:
+        n_dias = int(input("Inserte numero de días: "))
+        total = float(3.50+ (1.0 * n_dias))
+        print("EL TOTAL A PAGAR ES: $",total, "dolares")
+    elif opcion == 4:
+        n_dias = int(input("Inserte numero de días: "))
+        total = float(4.0+ (1.50 * n_dias))
+        print("EL TOTAL A PAGAR ES: $",total, "dolares")
+    elif opcion == 99:
+        break
+    else:
+        print("por favor, escriba una opción correcta. ")
+        print("\n")
 
