8e71b372a457c7f4749096f7e5a4da0e5c08e812
IsabelManiega <isabelmaniega@hotmail.com>
2022-07-28 10:41:42 +0200

    Ejercicios 23

---
+++ basico/Ejercicios_23.py
@@ -0,0 +1,95 @@
+# Ejercicio 1
+
+"""
+    Desarrolla el siguiente ejercicios de POO:
+
+   * Alumnos  -> Es la clase.
+   * __init__ -> Es el método init
+   * nombre, edad, asignatura y nota son las propiedades
+   * Instanciamos..
+   * los posibles alumnos (alumno1, alumno2, alumno3..) --> son los "objetos"
+   * y el.__init__ los inicializa
+
+   A continuación muestra la edad del alumno 2 y el alumno 3 y sus notas
+"""
+
+
+# Ejercicio 2
+
+"""
+    Escribir un programa que pregunte al usuario su edad
+    y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+"""
+
+# Ejercicio 3
+
+"""
+    Escribir un programa que pida al usuario una palabra y
+    luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
+"""
+
+# Ejercicio 4
+
+"""
+    Escribir un programa que pregunte el nombre completo del usuario en la consola y
+    después muestre por pantalla el nombre completo del usuario tres veces,
+    una con todas las letras minúsculas,
+    otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula.
+    El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+"""
+
+# Ejercicio 5
+
+"""
+    Los teléfonos de una empresa tienen el siguiente formato prefijo-número-extension donde el prefijo es el código del país +34,
+    y la extensión tiene dos dígitos (por ejemplo +34-913724710-56).
+    Escribir un programa que pregunte por un número de teléfono con este formato
+    y muestre por pantalla el número de teléfono sin el prefijo y la extensión.
+"""
+
+# Ejercicio 6
+
+"""
+    Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal,
+    y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
+"""
+
+# Ejercicio 7
+
+"""
+    Escribir un programa que pregunte por consola el precio de un producto en euros con dos decimales
+    y muestre por pantalla el número de euros y el número de céntimos del precio introducido.
+"""
+
+# Ejercicio 8
+
+"""
+    Escribir programa que genere y muestre por pantalla un DataFrame con los datos de la tabla siguiente:
+
+    Mes Ventas Gastos
+
+    Enero 30500 22000
+
+    Febrero 35600 23400
+
+    Marzo 28300 18100
+
+    Abril 33900 20700
+"""
+
+# Ejercicio 9
+
+"""
+    Escribir una función que reciba un DataFrame con el formato del ejercicio anterior,
+    una lista de meses, y devuelva el balance (ventas - gastos) total en los meses indicados.
+"""
+
+# Ejercicio 10
+
+"""
+    Escribir un programa que almacene las asignaturas de un curso
+    (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, 
+    pregunte al usuario la nota que ha sacado en cada asignatura,
+    y después las muestre por pantalla con el mensaje "Has sacado ASIGNATURA la nota de NOTA"
+    donde es cada una de las asignaturas de la lista y cada una de las correspondientes notas introducidas por el usuario.
+"""
