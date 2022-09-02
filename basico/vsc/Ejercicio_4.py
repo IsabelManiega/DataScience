33bfc7467dd9f3c63c094f516bfbe01a6d2d819d
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-02 12:51:30 +0200

    Ejercicio 4

---
+++ basico/vsc/Ejercicio_4.py
@@ -0,0 +1,47 @@
+# EJERCICIO 1
+
+"""
+    Crear un pequeño programa que calcule la multiplicación de 2 números (x, y)
+
+    x = 3, y = 5
+    x = 7, y = 3
+"""
+
+# a) Con una función (por ejemplo funcion_multiplicar)
+
+# b) Con la función lambda (Tal vez puedes ir a repasarlo)
+
+# c) Realizarlo con entrada de teclado (input)
+
+# EJERCICIO 2
+
+"""
+    -A-
+
+        Dado un string:
+
+        "Level"
+
+        ¿Es un palíndromo?
+"""
+
+"""
+    -B-
+
+        ¿Y este string?
+
+        "level"
+
+        Nota: "Es un palíndromo si se invierte el orden del string, el resultado es exactamente el mismo"
+"""
+
+# EJERCICIO 3
+
+"""
+    Dado 2 strings:
+
+    S1 = "Hi!"
+    S2 = "Hello!"
+
+    Imprimir las letras que son comunes
+"""
