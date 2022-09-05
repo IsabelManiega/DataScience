6cdfe15c45fe55022eaf1cfb27735133253098db
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-05 13:31:43 +0200

    Ejercicio 6

---
+++ basico/vsc/Ejercicio_6.py
@@ -0,0 +1,47 @@
+# EJERCICIO 1
+
+# Mínimo de una lista de números (lista de nombre "listado"): 30, 20, 10, 50, 40
+
+# 1) Escribe el listado e ímprimelo
+
+# 2) Prueba con min(listado)
+
+# 3) Realiza lo mismo pero con bucles y condicionales
+
+
+# EJERCICIO 2
+
+# Máximo de una lista de números (lista de nombre "listado"): 30, 20, 10, 50, 40
+
+# 1) Escribe el listado e ímprimelo
+
+# 2) Prueba con max(listado)
+
+# 3) Realiza lo mismo pero con bucles y condicionales
+
+# EJERCICIO 3
+
+# Ordena de menor a mayor un listado de números: 30, 20, 10, 50, 40 (de nombre "listado")
+
+# Pista: si quieres almacena esos números en una lista de nombre: "listado_ascendente"
+
+# 1) Escribe el listado e ímprimelo
+
+# 2) Prueba a usar sort()
+
+# 3) Realiza lo mismo pero con bucles y condicionales
+
+# EJERCICIO 4
+
+# Ordena de mayor a menor un listado de números: 30, 20, 10, 50, 40 (de nombre "listado")
+
+# 1) Escribe el listado e ímprimelo
+
+# 2) Prueba a usar sort()
+
+# 3) Realiza lo mismo pero con bucles y condicionales
+
+# EJERCICIO 5
+"""
+    Pendiente
+"""
