c544263ce3c97a2ed844e0a1132af143bb0e9d16
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-07 11:59:22 +0200

    Ejercicio 10

---
+++ basico/vsc/Ejercicio_10.py
@@ -0,0 +1,36 @@
+# EJERCICIO 1
+
+# Crea una matriz con ayuda numpy:
+
+# 1) Cuya matriz contenga 4 filas por 4 columnas de ceros
+
+# 2) Apartir de la matriz de ceros crea la matriz identidad
+
+
+# EJERCICIO 2
+
+# Crea una matriz con ayuda numpy:
+
+# Primera fila contenga: 3, 6, 8
+# Segunda fila contenga: 20, 5, 7
+# Tercera fila contenga: 10, 14, 1
+
+# 1) Crea la transpuesta de esta matriz
+
+# 2) Muestra el tamaño de la matriz
+
+# 3) Muestra las dimensiones
+
+# 4) ¿La matriz tiene valores menores de 0?
+
+# 5) ¿La matriz tiene algún valor mayor de 10?
+
+# 6) Coge una muestra de 5 valores de esa matriz usando linespace
+
+# 7) La matriz contiene el valor 7
+
+# 8) Crea una copia de esa matriz en una única dimensión
+
+# 9) Crea una copia de esa matriz en una única dimensión, en este caso usando un bucle y una lista vacia
+
+# 10) Realiza a esta última matriz creada con flatten la potencia de 3
