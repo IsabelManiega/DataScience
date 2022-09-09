1927b7242e8de686847d44a537eca8c0683e5496
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-09 11:03:27 +0200

    Ejercicio 13

---
+++ basico/vsc/Ejericicio_13.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# EJERCICIO 1
+
+# NO HACE FALTA HACERLO EN FORMATO FUNCIÓN!
+
+# Se pide por tanto:
+
+# -1- Leer el archivo train.csv de Titanic dataset con pandas (carpeta src)
+
+# -2- Imprimir algunas filas de la parte de arriba y otras de la parte del final
+
+# -3- Imprimir algunos parámetros estadísticos
+
+# -4- Ver si en todas las columnas tenemos el mismo número de datos (solo verlo)
+
+# -5- Ver el número de hombres y mujeres que hay
+
+# -6- Hacer alguna gráfica con pandas relativa al número de hombres y mujeres que hay
+
+# Si quieres hacer alguna cosa más también puedes
+
+
+# EJERCICIO 2
+"""
+    Dadas estas matrices:
+
+    * m1 = [[1, 10, 20], [30, 40, 50]]
+    * m2 = [[60, 80, 90,]]
+    * m3 = [[-2, 3, 5], [8, 6, 2]]
+"""
+
+# 1) Comprueba si todos los valores de las matrices son mayores de 0
+
+# 2) Si en la matriz m2 se encuentra el valor 80
+
+# 3) Selecciona de m1 las dos últimas columnas
+
+# 4) Concatena la m1 con m2, cuyo nombre de la matriz resultante sea m4
+
+# 5) Convierte m1 y m3 en "np.matrix" asignando el nombre de matriz_1 y matriz_3, respectivamente
+
+# 6) Realiza la resta, suma y producto de la matriz_1 y matriz_3
+
+# 7) Realiza las traza de la matriz de m4
+
+
+
+# EJERCICIO 3
+
+"""
+    * Numeros Primos: Crear un listado de los numeros primos menores de 30
+
+    Explicación inicial teórica
+
+    (véase Tema_7..)
+
+    Nota:
+
+    si quieres apendiza el número 2,
+
+    y a partir de ahí crea el algoritmo para apendizar los demas
+
+    (menores de 30 en todo caso)
+
+"""
+
+# 1) Pide por teclado un número y di si es o no primo
+
+# 2) Escribe los números primos menores de 30
+
+# 3) Numeros Primos: Crear un listado de los numeros primos menores de 200
+
+# 4) Números Primos: Haz un listado de números primos entre 50 y 100
+
+# 5) Numeros Primos: Haz un listado de los 10 primeros números primos
+    # Si puedes hazlo de más de una forma, no necesario aun así
+
