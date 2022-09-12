9b07dc7bf734b5bd5c0725ac28b2eb013f1b131b
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-12 10:06:42 +0200

    Ejercicio 15

---
+++ basico/vsc/Ejercicio_15.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+# EJERCICIO 1
+
+
+# 1) Crea una lista de nombre "Concursante" que contenga los siguientes valores:
+    # "Juan", "Pedro", "Andrea", "Luis", "Ana", "Lara", "Jose", "Estefania"
+
+# 2) Crea una lista de nombre "Resultados" que contenga los siguientes valores:
+    # 20, 30, 50, 20, 10, 5, 60, 40
+
+# 3) Crea un diccionario con los concursantes y los resultados.
+
+# 4) Crea un dataframe vacio y apendiza los concursantes y los resultados mediante el empleo de un bucle for
+
+# 5) Con ayuda de loc filtra los resultados obtenidos desde Pedro hasta Lara.
+
+# 6) Con ayuda de loc filtra los resultados obtenidos mayores o iguales a 40
+
+# 7) Muestra el concursante con la mayor puntuación
+
+# 8) Muestra el concursante con la menor puntuación
+
+# 9) Modifica con la ayuda de loc los valores 20 por 0
+
+# 10) Modifica Concursante "Juan" su puntuación por "None" con ayuda de .loc
+
+# EJERCICIO 2 (3)
+
+"""
+    Escribe un programa que pida dos palabras y diga si riman o no.
+    Si coinciden las tres últimas letras tiene que decir que riman.
+    Si coinciden sólo las dos últimas tiene que decir que riman un poco y si no, que no riman.
+"""
