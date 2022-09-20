f356a76b9b20b9dcc923bc99636175c46885c46a
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-20 14:33:01 +0200

    Ejercicio 3

---
+++ poo/vsc/Ejercicio_3.py
@@ -0,0 +1,36 @@
+# EJERCICIO 1
+
+"""
+    Crearemos una clase llamada NumeroComplejo.
+    Este tipo tiene un atributo x para la coordenada en x e y para la coordenada en y.
+    Representa un número complejo de la forma (x, y).
+"""
+
+# EJERCICIO 2
+
+"""
+    Ahora defina dentro de la clase NumeroComplejo un función imprimir
+    donde muestre los valores de x e y.
+"""
+
+# EJERCICIO 3
+
+"""
+    Define la función str para la clase NumeroComplejo
+    para poder imprimir usando la función print.
+"""
+
+# EJERCICIO 4
+
+"""
+    Define una función que compara dos números complejos,
+    ya que si dos objetos distintos tienen sus atributos iguales,y
+    sino NO se consideran iguales.
+"""
+
+# EJERCICIO 5
+
+"""
+    Realiza un método que sume dos numeros complejos sin modificiar los objetos originales,
+    ya que se retorna un nuevo numero NumeroComplejo.
+"""
