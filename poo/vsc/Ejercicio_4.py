f3c67adf2e90408fc111bdd3d88affeb3112192c
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-21 10:35:00 +0200

    Ejercicio 4

---
+++ poo/vsc/Ejercicio_4.py
@@ -0,0 +1,22 @@
+# EJERCICIO 1
+
+"""
+    Crea una clase persona. Sus atributos deben ser su nombre y su edad.
+    Además crea un método cumpleaños, que aumente en 1 la edad de la persona.
+"""
+
+# EJERCICIO 2
+
+"""
+    Para la clase anterior definir el método str.
+    Debe retornar al menos el nombre de la persona.
+"""
+
+# EJERCICIO 3
+
+"""
+    Extender la clase persona agregando un atributo saldo y un método transferencia(self, persona2, monto).
+    El saldo representa el dinero que tiene la persona.
+    El método transferencia hace que la Persona que llama el método le transfiera la cantidad monto al objeto persona2.
+    Si no tiene el dinero suficiente no se ejecuta la acción.
+"""
