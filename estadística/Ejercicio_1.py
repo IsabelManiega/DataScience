10fbb9a0bcbb2c9b8625ad58b1aadf67347c9394
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-28 10:56:50 +0200

    Ejercicio 1

---
+++ estadística/Ejercicio_1.py
@@ -0,0 +1,13 @@
+# EJERCICIO 1
+
+# Dada estos valores: 15 16 17 16 21 22 15 16 15 17 16 22 14 13 14 14 15 15 14 15 16 10 19 15 15 22 24 25 15 16
+
+# 1) Realiza un Histograma con tamaño de barra 2.
+
+# 2) ¿Cuál es el número que más se repite?
+
+# 3) ¿Qué pasa si cambiamos a tamaño de barra 5?
+
+# 4) ¿Qué pasa si cambiamos a tamaño de barra 20?
+
+# 5) ¿Qué parece indicar el sesgo en la distribución?
