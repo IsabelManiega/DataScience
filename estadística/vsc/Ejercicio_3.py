8df4a9700fa17b0d735ee8d7a595363372bf2249
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-29 11:24:44 +0200

    Ejercicio 3

---
+++ estadística/vsc/Ejercicio_3.py
@@ -0,0 +1,17 @@
+# EJERCICIO 1
+
+"""
+    En 2007-2008, la altura promedio de un jugador de baloncesto profesional fue de 2,00 metros con una desviación estándar de 0,02 metros. 
+    Harrison Barnes es un jugador de baloncesto que mide 2,03 metros. ¿Qué porcentaje de jugadores son más altos que Barnes?
+"""
+
+# EJERCICIO 2
+
+# Chris Paul mide 1,83 metros. ¿Qué proporción de jugadores de baloncesto se encuentran entre las alturas de Paul y Barnes?
+
+# EJERCICIO 3
+
+"""
+    El 92 % de los candidatos obtuvo una puntuación tan buena o peor que la de Steve.
+    Si el puntuación promedio fue 55 con una desviación estándar de 6 puntos, ¿cuál fue el puntuación de Steve?
+"""
