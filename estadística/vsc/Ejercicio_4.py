4ac1db6ba5229d0e22d021ddc35b8e9b1abdf6d6
IsabelManiega <isabelmaniega@hotmail.com>
2022-09-30 09:49:17 +0200

    Ejercicio 4

---
+++ estadística/vsc/Ejercicio_4.py
@@ -0,0 +1,18 @@
+# EJERCICIO 1
+
+"""
+    Una población distribuida normalmente tiene una media de 100 y una desviacion estándar de 20.
+    ¿ Cuál es la puntuación Z de una media muestral de 110, tomada de una muestra de tamaño 4?
+"""
+
+# EJERCICIO 2
+
+"""
+    El tiempo medio conocido que se tarda en entregar una pizza es de 22.5 minutos con una desviación estándar de 2 minutos.
+    Pedí pizza todas las semanas durante las últimas 10 semanas y obtuve un tiempo de 21.5 minutos.
+    ¿Cuál es la probabilidad de obtener este promedio?
+"""
+
+# EJERCICIO 3
+
+# Si sigo pidiendo pizzas por la toda la eternidad. ¿A qué nivel puedo esperar que se acerque este promedio?
