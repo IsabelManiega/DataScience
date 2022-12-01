d945230c405979b3b6b4ea63b840256e346ab5b5
IsabelManiega <isabelmaniega@hotmail.com>
2022-12-01 12:49:32 +0100

    ejercicio

---
+++ dashboard/exercise_1.py
@@ -0,0 +1,12 @@
+# Realizar el ejercicio con yfinance
+
+# 1) Poner la pantalla en modo apaisado (que ocupe todo el espacio)
+# 2) Mostrar los datos de la tabla usando la opcion de streamlit dataframe
+
+# 3) Escoger para mostrar la tabla y el gráfico la columna 
+# que quiera el usario usando un radio button
+
+# 4) Visualizar la información usando plotly: 
+# necesitamos modificar el dataset, para ello usaremos:
+# data = data.rename_axis('Date').reset_index()
+# para modificar el index para la fecha.
